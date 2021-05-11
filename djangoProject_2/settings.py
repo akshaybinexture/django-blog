01a7220a9be2b36cc16e52cb7b9210e25a34966f
akshaybinexture <akshayb.inexture@gmail.com>
2021-05-11 17:49:18 +0530

    feat:changes in project

--- djangoProject_2/settings.py
+++ djangoProject_2/settings.py
@@ -163,6 +163,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 SITE_ID = 3  # for social authentication
 
